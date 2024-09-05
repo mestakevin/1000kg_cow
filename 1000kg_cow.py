146f89aade80c380fee47156f20ac215b9a76b7c
mestakevin <kmmesta@syr.edu>
2024-09-05 13:37:54 -0400

    setting up initial conditions for probelm

--- 1000kg_cow.py
+++ 1000kg_cow.py
@@ -1,1 +1,17 @@
 #this is a 1000kg cow
+
+import numpy
+import matplotlib
+
+##-----------------------------------------------------------------------##
+#Inital conditions
+mass_cow = 1000 #kg
+grav = 9.8 #m/s^2
+init_pos_cow = [0,100]
+init_vel_cow = [10,10]
+wind_res_cons = 0.01
+
+##-----------------------------------------------------------------------##
+
+
+
