6f8e244bd419813854c8bcec061759014d4c0a2e
Sara-Talebi <stalebi@g.syr.edu>
2024-09-05 14:25:03 -0400

    adding function to calc current position and velocity

--- 1000kg_cow.py
+++ 1000kg_cow.py
@@ -5,6 +5,18 @@ import matplotlib as plt
 
 
 ##-----------------------------------------------------------------##
+def curr_pos_vel_calc(position,velocity,f_tot,mass,dt):
+    #f=ma
+    accel = f_tot / mass
+    
+    #v=v0+adt
+    curr_vel = velocity + (accel * dt)
+    
+    #r=r0+vdt
+    curr_pos = position + (velocity * dt)
+    
+    return curr_vel, curr_pos
+##-----------------------------------------------------------------##
 def tot_force_vec(velocity,mass,g,wind_res_cons):
     f_grav = -mass * g
     
@@ -21,8 +33,8 @@ def tot_force_vec(velocity,mass,g,wind_res_cons):
 ##-----------------------------------------------------------------------##
 def test_function():
     #Inital conditions
-    mass          = 1000 #kg
-    grav          = 9.8 #m/s^2
+    mass          = 1000 	#kg
+    grav          = 9.8 	#m/s^2
     init_pos_cow  = [0,100] 
     init_vel_cow  = [10,10]
     wind_res_cons = 0.01
