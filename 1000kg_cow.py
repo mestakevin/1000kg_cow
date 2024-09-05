8b5a63feb6fd28e9b509b93d8c0599eec76c551c
Sara-Talebi <stalebi@g.syr.edu>
2024-09-05 13:52:24 -0400

    adding test function

--- 1000kg_cow.py
+++ 1000kg_cow.py
@@ -1,16 +1,22 @@
 #this is a 1000kg cow
 
-import numpy
-import matplotlib
+import numpy as np
+import matplotlib as plt
 
 ##-----------------------------------------------------------------------##
-#Inital conditions
-mass_cow = 1000 #kg
-grav = 9.8 #m/s^2
-init_pos_cow = [0,100]
-init_vel_cow = [10,10]
-wind_res_cons = 0.01
-
+def test_function():
+    #Inital conditions
+    mass_cow = 1000 #kg
+    grav = 9.8 #m/s^2
+    init_pos_cow = [0,100] 
+    init_vel_cow = [10,10]
+    wind_res_cons = 0.01
+    time	      = 0.0
+    dt            = 0.1
+    g	      = 9.8 #m/s^2
+    
+    position = np.array(ini_pos_vec)
+    velocity = np.array(ini_vel_vec)
 ##-----------------------------------------------------------------------##
 
 
